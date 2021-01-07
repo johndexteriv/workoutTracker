255df7f3d69e9029815cf7dfa1577fc2b1bbdd23
John Garner <jdextergarneriv@gmail.com>
2021-01-07 14:50:57 -0500

    created api and html route files

---
+++ routes/api-routes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const Workout = require("../models/workoutModel.js");
+
+module.exports = router;
+
+router.get("/api/");
--- routes/api.js
+++
@@ -1,4 +0,0 @@
-const router = require("express").Router();
-const Workout = require("../models/workout.js");
-
-module.exports = router;
