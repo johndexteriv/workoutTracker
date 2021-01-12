1b13f5750aafa63f0e9dce435fb76af647bb0460
John Garner <jdextergarneriv@gmail.com>
2021-01-11 19:53:39 -0500

    adjusted schema for exercise key to hold data array. Completed get, put and post api routes.

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -6,13 +6,29 @@ const workoutSchema = new Schema({
 		type: Date,
 		default: Date.now,
 	},
-	type: String,
-	name: String,
-	duration: Number,
-	weight: Number,
-	reps: Number,
-	sets: Number,
-	distance: Number,
+	exercises: [
+		{
+			type: {
+				type: String,
+				trim: true,
+				required: "Please select a workout type",
+			},
+			name: {
+				type: String,
+				trim: true,
+				required: "Please input a workout name",
+			},
+			duration: {
+				type: Number,
+				trim: true,
+				required: "Please input a workout duration",
+			},
+			weight: { type: Number },
+			reps: { type: Number },
+			sets: { type: Number },
+			distance: { type: Number },
+		},
+	],
 });
 
 var Workout = mongoose.model("Workout", workoutSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,24 +3,47 @@ const Workout = require("../models/workoutModel.js");
 
 module.exports = router;
 
-router.post("/api/addExercise", ({ body }, res) => {
-	Workout.create([body], (err, result) => {
+router.post("/api/workouts", ({ body }, res) => {
+	Workout.create(body)
+		.then((dbTransaction) => {
+			res.json(dbTransaction);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+	Workout.findByIdAndUpdate({ _id: params.id }, { $push: { exercises: body } })
+		.then((dbTransaction) => {
+			res.json(dbTransaction);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+//Finish range apiroute
+
+// router.get("/api/workouts", (req, res) => {
+// 	Workout.find().limit(5),
+// 		(err, data) => {
+// 			if (err) {
+// 				throw err;
+// 			} else {
+// 				return res.json(data);
+// 			}
+// 		};
+// });
+
+query.limit(20);
+
+router.get("/api/workouts", (req, res) => {
+	Workout.find({}, (err, data) => {
 		if (err) {
-			res.json(err);
+			throw err;
 		} else {
-			res.json(result);
+			return res.json(data);
 		}
 	});
 });
-
-// router.get("/exercise", (req, res) => {
-// 	Workout.find({}, (err, data) => {
-// 		if (err) {
-// 			throw err;
-// 		} else {
-// 			return res.json(data);
-// 		}
-// 	});
-// });
-
-// or router.get("/exercise");
